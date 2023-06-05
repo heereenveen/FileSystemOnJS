c6fe545add5e6f30079e2ee42e1370ea72b57c3a
RostislavHundarev <115458168+RostislavHundarev@users.noreply.github.com>
2023-06-05 09:54:31 +0200

    the program is initialized

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
